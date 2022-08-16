c13facee8dcfa312d79b69ab8d4e18555e7119a2
PaulOkoh <pauldokoh@gmail.com>
2022-08-16 18:15:44 -0500

    add hover function

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-/*
-Functions/rqmts
-1.Create grid: rows and columns
-2.Clear grid
-3.modify grid counts
-4. change color on hover
-5. use random colors on hover
- */
+
 
 function createGrids(x) {
   for(let rows = 0; rows < x; rows++){
     for(let columns = 0; columns < x; columns++){
-      const box = document.getElementById("box");
+      const box = document.querySelector("#box");
       const div = document.createElement("div");
       box.appendChild(div);
       div.classList.add("grid");
-      div.setAttribute("style", `height: ${480/x}px; width: {480/x}px`);
+      div.setAttribute("style", `height: ${480/x}px; width: ${480/x}px`);
     }
   }
 }
@@ -27,10 +20,21 @@ function changeGridCount() {
   clearGrid();
   let columnCount = prompt("enter grid count");
   createGrids(columnCount);
-  
-
+  changeColorOnHover();
 }
 
+function changeColorOnHover() {
+  let grids = document.querySelectorAll(".grid");
+  grids.forEach(function(div) {
+    div.addEventListener("mouseover", function(e) {
+      const color = "#000";
+      e.target.style.background = color;
+    })
+  })
+}
+
+
+document.querySelector("#restart").addEventListener("click", changeGridCount);
 
 
 
